9955da6b1a6807298165134a4d6f74af722f594e
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-10-10 01:50:57 -0500

    Cambios

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,11 @@ module.exports = {
     main: ["@babel/polyfill", "./src/app/scripts/main.js"],
     home: ["@babel/polyfill", "./src/app/scripts/home.js"],
   },
-  output:{
+  output: {
     filename: 'js/[name].bundle.js',
     path: path.join(__dirname, 'public', 'scripts'),
     chunkFilename: '[id].[chunkhash].js'
-},
+  },
 
   devServer: {
     port: 5501,
@@ -32,6 +32,23 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      // {
+      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //   use: [
+      //     { 
+      //       loader: 'file-loader',
+      //       options: {
+      //         name: '[name].[ext]',
+      //         outputPath: 'assets/',
+      //         publicPath: 'assets/'
+      //       }
+      //     }
+      //   ]
+      // },
+      // {
+      //   test: /\.html$/i,
+      //   use: ['html-loader']
+      // },
     ]
   },
   plugins: [
